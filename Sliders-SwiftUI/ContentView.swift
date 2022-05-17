90688c2e6c509e19cdc4a3b13becad8a7eb08510
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-05-17 19:46:38 +0700

    Initial Commit

---
+++ Sliders-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sliders-SwiftUI
+//
+//  Created by Алексей Синяговский on 17.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sliders-SwiftUI/Sliders_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sliders_SwiftUIApp.swift
+//  Sliders-SwiftUI
+//
+//  Created by Алексей Синяговский on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Sliders_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
